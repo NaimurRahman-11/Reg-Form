867b6d7c477c486b6e45189c16aae052a1e5db15
Naimur Rahman <nirzon.naim@gmail.com>
2022-05-28 23:59:01 +0800

    JavaScript Added

---
+++ script.js
@@ -0,0 +1,37 @@
+class signUp{
+
+    constructor(firstName,lastName,email,password,gender){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+
+    }
+
+    showInfo(){
+        console.log(this.firstName, this.lastName, this.email, this.password, this.gender);
+    }
+
+
+}
+
+function getElementFromFrontEnd(){
+    FirstName = document.getElementById("firstname").value,
+    LastName = document.getElementById("lastname").value,
+    Email = document.getElementById("email").value,
+    Password = document.getElementById("password").value
+
+    if(document.getElementById('male').checked){
+       gender=document.getElementById('male').value;
+    }
+    else{
+        gender=document.getElementById('female').value;
+    }
+}
+
+function printData(){
+    getElementFromFrontEnd();
+    var s = new signUp(FirstName, LastName,Email,Password,gender);
+    s.showInfo();
+}
